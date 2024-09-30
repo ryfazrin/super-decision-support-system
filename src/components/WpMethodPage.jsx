6589cebb7f15ff8f8f7ef4fa7f8a65237b3f9e2c
ryfazrin <riansera515@gmail.com>
2024-09-30 19:41:18 +0700

    feat: add url WP method

--- src/components/WpMethodPage.jsx
+++ src/components/WpMethodPage.jsx
@@ -3,14 +3,14 @@ import React from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { Form, InputNumber, Button, message, Typography, Card, Space } from 'antd';
 import { useMutation } from '@tanstack/react-query';
-import axios from 'axios';
+import axiosInstance from '../lib/axiosInstance';
 
 const { Title } = Typography;
 
 // Fungsi untuk mengirim data ke server
 const postData = async (data) => {
   // Ganti URL dengan endpoint API Anda
-  return axios.post('https://api.example.com/submit', data);
+  return axiosInstance.post('/public/dss', data);
 };
 
 function WpMethodPage() {
@@ -19,6 +19,19 @@ function WpMethodPage() {
 
   // Mengakses data yang diteruskan dari HomePage
   const { parameters } = location.state.values || {};
+
+  // Menggunakan React Query untuk menangani mutasi data
+  const { mutate, isLoading } = useMutation(postData, {
+    onSuccess: (data) => {
+      message.success('Data berhasil dikirim!');
+      // Optionally, reset form atau navigasi ke halaman lain
+      // form.resetFields();
+      navigate('/result', { state: { data: data.data } });
+    },
+    onError: (error) => {
+      message.error(`Gagal mengirim data: ${error.message}`);
+    }
+  });
 
   // Jika data tidak ditemukan, tampilkan pesan error dan opsi untuk kembali
   if (!parameters) {
@@ -38,37 +51,16 @@ function WpMethodPage() {
     initialWeights[param] = 0; // Nilai awal, bisa disesuaikan
   });
 
-  // Menggunakan React Query untuk menangani mutasi data
-  const { mutate, isLoading } = useMutation(postData, {
-    onSuccess: () => {
-      message.success('Data berhasil dikirim!');
-      // Optionally, reset form atau navigasi ke halaman lain
-      // form.resetFields();
-    },
-    onError: (error) => {
-      message.error(`Gagal mengirim data: ${error.message}`);
-    }
-  });
-
   // Handler untuk submit form
   const onFinish = (values) => {
-    // Transformasi data ke format yang diinginkan
-    // const transformedData = {
-    //   weight: {
-    //     values: parameters.map(param => values.weight[param])
-    //   }
-    // };
-
-    console.log({
+    // Kirim data ke API
+    mutate({
       ...location.state.values,
       weight: {
         method: 'DIRECT',
         values: parameters.map(param => values.weight[param])
       }
     });
-
-    // Kirim data ke API
-    // mutate(transformedData);
   };
 
   return (
