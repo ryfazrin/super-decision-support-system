e804f884140e2cb846a844230b6030dd050084fa
ryfazrin <riansera515@gmail.com>
2024-10-03 21:24:40 +0700

    refactor: update baseURL to https

--- src/lib/axiosInstance.js
+++ src/lib/axiosInstance.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'http://ec2-3-26-197-201.ap-southeast-2.compute.amazonaws.com:8080',  // Base URL
+    baseURL: 'https://zulvani.tech:8080',  // Base URL
     timeout: 1000,  // Request timeout
 });
 
